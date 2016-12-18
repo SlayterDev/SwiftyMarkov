0a844385dcff464202f17c20310ba9ddc052b451
Brad Slayter <BradleySlayter@my.unt.edu>
2016-12-18 09:21:23 -0600

    Initial Commit

---
+++ SwiftyMarkov/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftyMarkov
+//
+//  Created by Bradley Slayter on 12/18/16.
+//  Copyright © 2016 Flipped Bit. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
